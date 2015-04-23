3ed82a7d50cbbd5c6c133d9241dc9698adcdb50f
alexjames <alx.james@gmail.com>
2015-04-23 23:24:55 +0530

    Support duplicates in previous program

--- Miscellaneous/FindMinSortedRotatedArray.c
+++ Miscellaneous/FindMinSortedRotatedArray.c
@@ -26,9 +26,28 @@ int findmin(int A[], int low, int high)
     {
         return findmin(A, low, mid);
     }
-    else
+    else if (A[mid] > A[high])
     {
         return findmin(A, mid, high);
+    }
+    else
+    {
+        if (A[mid] != A[low])
+        {
+            return findmin(A, low, mid);
+        }
+        else
+        {
+            int ret = findmin(A, low, mid);
+            if (ret != A[mid])
+            {
+                return ret;
+            }
+            else
+            {
+                return findmin(A, mid, high);
+            }
+        }
     }
 }
 
@@ -45,7 +64,7 @@ void test(int A[], int size)
 
 int main()
 {
-    int A[] = {1,2,3,4,5,6,7};
+    int A[] = {10,10,1,10,10,10,10};
     test(A, CountOf(A));
     int B[] = {8,9,1,2,3,4,5,6,7};
     test(B, CountOf(B));
@@ -53,5 +72,7 @@ int main()
     test(C, CountOf(C));
     int D[] = {3,1,2};
     test(D, CountOf(D));
+    int E[] = {4,1,4,4,4,4,4,4,4,4};
+    test(E, CountOf(E));
     return 0;
 }
